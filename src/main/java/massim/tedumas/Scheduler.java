d7ad72cdeb5db55b501ceef84b01a4d04eb5e63d
bavenoglu <bavenoglu@gmail.com>
2022-03-04 17:21:12 +0300

    Agent data stored permanently.

--- src/main/java/massim/tedumas/Scheduler.java
+++ src/main/java/massim/tedumas/Scheduler.java
@@ -11,17 +11,12 @@ import eis.iilang.Percept;
 import massim.eismassim.EnvironmentInterface;
 import massim.tedumas.agents.Agent;
 import massim.tedumas.agents.BasicAgent;
-
 import org.apache.logging.log4j.LogManager;
-
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
-import org.kie.api.runtime.KieSession;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
--- src/main/java/massim/tedumas/agents/BasicAgent.java
+++ src/main/java/massim/tedumas/agents/BasicAgent.java
@@ -1,15 +1,22 @@
 package massim.tedumas.agents;
 
+import bitronix.tm.TransactionManagerServices;
+import bitronix.tm.resource.jdbc.PoolingDataSource;
 import eis.iilang.*;
 import massim.eismassim.EnvironmentInterface;
 import massim.tedumas.MailService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.drools.persistence.api.PersistenceContextManager;
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
+import org.kie.api.persistence.jpa.KieStoreServices;
+import org.kie.api.runtime.Environment;
+import org.kie.api.runtime.EnvironmentName;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import javax.persistence.Persistence;
 import java.util.List;
 
 /**
@@ -19,10 +26,11 @@ public class BasicAgent extends Agent {
     private static final Logger
             logger = LogManager.getLogger(BasicAgent.class);
     private int lastID = -1;
-    KieServices kieServices;
-    KieContainer kContainer;
+    KieServices KServices;
+    KieContainer KContainer;
     KieBase KBase;
     KieSession KSession;
+    private PersistenceContextManager jpm;
     /**
      * Constructor.
      * @param name    the agent's name
@@ -30,12 +38,31 @@ public class BasicAgent extends Agent {
      */
     public BasicAgent(String name, MailService mailbox, EnvironmentInterface ei) {
         super(name, mailbox);
-        kieServices = KieServices.Factory.get();
-        kContainer = kieServices.getKieClasspathContainer();
 
-        KBase = kContainer.getKieBase("KBase" + name);
-        KSession = kContainer.newKieSession("KSession" + name);
+        PoolingDataSource ds = new PoolingDataSource();
+        ds.setUniqueName( "jdbc/DS" + name );
+        ds.setClassName( "org.h2.jdbcx.JdbcDataSource" );
+        ds.setMaxPoolSize( 3 );
+        ds.setAllowLocalTransactions( true );
+        ds.getDriverProperties().put( "user", "sa" );
+        ds.getDriverProperties().put( "password", "sasa" );
+        ds.getDriverProperties().put( "URL", "jdbc:h2:file:./jdbc/DB" + name );
+        ds.init();
+
+        KServices = KieServices.Factory.get();
+
+        Environment env = KServices.newEnvironment();
+        env.set( EnvironmentName.ENTITY_MANAGER_FACTORY,
+                Persistence.createEntityManagerFactory( "Persist" + name));
+        env.set( EnvironmentName.TRANSACTION_MANAGER,
+                TransactionManagerServices.getTransactionManager() );
+
+        KContainer = KServices.getKieClasspathContainer();
+        KieStoreServices kstore = KServices.getStoreServices();
+        KSession = kstore.newKieSession(KContainer.getKieBase("KBase" + name), null, env);
         KSession.setGlobal("eis", ei);
+        KSession.fireAllRules();
+
     }
 
     @Override
@@ -52,6 +79,7 @@ public class BasicAgent extends Agent {
             logger.info(percept);
             KSession.insert(percept);
             KSession.fireAllRules();
+
             if (percept.getName().equals("actionID")) {
                 Parameter param = percept.getParameters().get(0);
                 if (param instanceof Numeral) {
